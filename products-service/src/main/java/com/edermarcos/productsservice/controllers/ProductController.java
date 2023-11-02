00d48401d4bdd9b1c3aa4a23be05f9c458c48e6c
Eder Marcos <eder.marcos.lara@gmail.com>
2023-11-01 18:02:55 -0600

    Add the rest of metdos to have the crud of products

--- products-service/src/main/java/com/edermarcos/productsservice/controllers/ProductController.java
+++ products-service/src/main/java/com/edermarcos/productsservice/controllers/ProductController.java
@@ -1,10 +1,11 @@
 package com.edermarcos.productsservice.controllers;
 
 import com.edermarcos.productsservice.model.dtos.ProductRequest;
-import com.edermarcos.productsservice.model.dtos.ProductResponse;
+import com.edermarcos.productsservice.model.entities.ProductEntity;
 import com.edermarcos.productsservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,13 +19,29 @@ public class ProductController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public void create(@RequestBody ProductRequest productRequest) {
-    productService.create(productRequest);
+  public ProductEntity create(@RequestBody ProductRequest productRequest) {
+    return productService.save(productRequest);
   }
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public List<ProductResponse> getAll() {
+  public List<ProductEntity> getAll() {
     return productService.getAll();
+  }
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<ProductEntity> getById(@PathVariable Long id) {
+    return productService.getById(id);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<ProductEntity> update(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+    return productService.update(id, productRequest);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    return productService.delete(id);
   }
 }
--- products-service/src/main/java/com/edermarcos/productsservice/services/ProductService.java
+++ products-service/src/main/java/com/edermarcos/productsservice/services/ProductService.java
@@ -1,43 +1,60 @@
 package com.edermarcos.productsservice.services;
 
 import com.edermarcos.productsservice.model.dtos.ProductRequest;
-import com.edermarcos.productsservice.model.dtos.ProductResponse;
 import com.edermarcos.productsservice.model.entities.ProductEntity;
 import com.edermarcos.productsservice.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
   @Autowired
   private ProductRepository productRepository;
 
-  public void create(ProductRequest productRequest) {
-    var product = ProductEntity.builder()
-        .sku(productRequest.getSku())
-        .name(productRequest.getName())
-        .description(productRequest.getDescription())
-        .price(productRequest.getPrice())
-        .status(productRequest.getStatus())
-        .build();
-
-    productRepository.save(product);
+  public ProductEntity save(ProductRequest productRequest) {
+    var product = getProductEntity(productRequest);
+    return productRepository.save(product);
   }
 
-  public List<ProductResponse> getAll() {
-    var products = productRepository.findAll();
-    return products.stream().map(this::mapToProductEntity).toList();
+  public List<ProductEntity> getAll() {
+    return productRepository.findAll();
   }
 
-  private ProductResponse mapToProductEntity(ProductEntity productEntity) {
-    return ProductResponse.builder()
-        .sku(productEntity.getSku())
-        .name(productEntity.getName())
-        .description(productEntity.getDescription())
-        .price(productEntity.getPrice())
-        .status(productEntity.getStatus())
-        .build();
+  public Optional<ProductEntity> getById2(Long id) {
+    return productRepository.findById(id);
+  }
+
+  public ResponseEntity<ProductEntity> getById(Long id) {
+    return productRepository.findById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+  }
+
+  public ResponseEntity<ProductEntity> update(Long id, ProductRequest productRequest) {
+    return productRepository.findById(id).map(savedProduct -> {
+      savedProduct.setSku(productRequest.getSku());
+      savedProduct.setName(productRequest.getName());
+      savedProduct.setDescription(productRequest.getDescription());
+      savedProduct.setPrice(productRequest.getPrice());
+      savedProduct.setStatus(productRequest.getStatus());
+
+      productRepository.save(savedProduct);
+      return ResponseEntity.ok(savedProduct);
+    }).orElse(ResponseEntity.notFound().build());
+  }
+
+  public ResponseEntity<Void> delete(Long id) {
+    return productRepository.findById(id).map(savedProduct -> {
+      productRepository.delete(savedProduct);
+      return ResponseEntity.ok().<Void>build();
+    }).orElse(ResponseEntity.notFound().build());
+  }
+
+  private ProductEntity getProductEntity(ProductRequest productRequest) {
+    return ProductEntity.builder().sku(productRequest.getSku()).name(productRequest.getName()).description(productRequest.getDescription()).price(productRequest.getPrice()).status(productRequest.getStatus()).build();
   }
 }
